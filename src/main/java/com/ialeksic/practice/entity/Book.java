c0c6dd1d3b48a6f6358759c58f6dc51e4dbb6f6d
ivan-aleksic <ivan.aleksic582@gmail.com>
2022-01-17 14:47:18 +0100

    -added attributes for book entity -added annotations on book entity -added application properties for book database

--- src/main/java/com/ialeksic/practice/entity/Book.java
+++ src/main/java/com/ialeksic/practice/entity/Book.java
@@ -1,4 +1,29 @@
 package com.ialeksic.practice.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_sequence")
+    @SequenceGenerator(name = "book_sequence", allocationSize = 10)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String isbn;
+    @Column
+    private String authorFirstName;
+    @Column(name="pero")
+    private String authorLastName;
 }
